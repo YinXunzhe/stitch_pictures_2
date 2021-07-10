00d35f755abcbfde54436c5d1b2e43e8f1da0796
YinXunzhe <1728591569@qq.com>
2021-07-10 18:07:59 +0800

    优化拼图思路：由于部分灰色图标文件的名字不规范，之前是根据文件名分别拼接，现在优化为对命名不规范的灰色图标文件的名字进行规范化后统一拼接

--- CANoe_Pic_Stitching.py
+++ CANoe_Pic_Stitching.py
@@ -1,15 +1,40 @@
 import os
 from PIL import Image
 
-#根据文件名的对应关系，将“灰色”文件夹和“蓝色”文件夹内
-#不同色彩的对应图标拼接成一张图存放到“Elements“文件夹中
-
-path1=os.path.abspath('.') #获取当前文件夹的绝对路径
+path_common=os.path.abspath('.') #获取当前文件夹的绝对路径
 path_grey = '\灰色'
 path_blue = '\蓝色'
-path_output = '\Elements'
+#存放拼接后图像的文件夹
+path_output = '\拼图'
 
-def pinjie(key, images, path):
+
+def main():
+    """
+    根据文件名的对应关系，将“灰色”文件夹和“蓝色”文件夹内不同色彩的图标image1和imgae2
+    拼接成以(image1,image1,image2)方式排布的图标，最后创建并存放到“拼图“文件夹中
+    """
+    #检查输出路径是否存在，若不存在则创建
+    if not os.path.exists(path_common+path_output):
+        os.makedirs(path_common+path_output)
+
+    #获取灰色图标文件夹中图片的文件路径和文件名
+    for root, dirs, filenames in os.walk(path_common+path_grey):
+        for name in filenames:
+            # 根据图标名字name，拼合出灰色图标的完整的文件路径
+            file_grey = os.path.join(root, name)
+
+            #由于个别灰色图标的文件名不规范，需进行规范化
+            name=name.split('.')[0].strip(' (1)')+'.png'
+
+            #蓝色图标文件夹中对应的图标完整路径
+            file_blue = os.path.join(path_common+path_blue, name)
+
+            if os.path.exists(file_blue):
+                stitch_pictures(name, (file_grey, file_grey, file_blue), path_common + path_output)
+            else:
+                print(f"{file_blue} doesn't exist!\n")
+
+def stitch_pictures(key, images, path):
     """ 拼接图像
         key-文件名
         images-要拼接的图像序列
@@ -32,36 +57,7 @@ def pinjie(key, images, path):
     path = os.path.join(path, f'{key}.jpg')
     result.save(path)
 
-def main():
-    #获取灰色图标文件夹中图片的文件路径和文件名
-    for root, dirs, filenames in os.walk(path1+path_grey):
-        for name in filenames:
-            #排除文件名中包含括号的图片
-            if '(' in name:
-                pass
-            else:
-                # 根据图标名字name，拼合出完整的文件路径
-                file_path = os.path.join(root, name)
-                #因为灰色图标文件夹中图标的文件名有两种格式，因此图片的完整路径也有以下两种
-                file0 = file_path
-                file1 = os.path.join(root, f"{name.split('.')[0]} (1).png")
-
-                #蓝色图标文件夹中同名的图标完整路径
-                file2 = os.path.join(path1+path_blue, name)
-
-                print('file0', file0)
-                print('file1', file1)
-                print('file2', file2)
-
-                #如果存在file1，先去看file2是否存在，若存在就拼接file1、file2
-                if os.path.exists(file1):
-                    print('nice1')
-                    if os.path.exists(file2):
-                        print('nice2')
-                        pinjie(name.split('.')[0], (file1, file1, file2),path1+path_output)
-                #如果存在file2，就直接拼接file0、file2
-                elif os.path.exists(file2):
-                    pinjie(name.split('.')[0], (file0, file0, file2),path1+path_output)
+    print(f'{key}.jpg saved!')
 
 if __name__ == '__main__':
     main()
